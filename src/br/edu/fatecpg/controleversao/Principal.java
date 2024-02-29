4eb792af7a2f3227a8cf2d2c0c08c03cb99de0e9
Ale Lima <alessandrofpl@gmail.com>
2024-02-29 16:19:06 -0300

    Criaçao do projeto java

---
+++ src/br/edu/fatecpg/controleversao/Principal.java
@@ -0,0 +1,10 @@
+package br.edu.fatecpg.controleversao;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Mundo");
+	}
+
+}
