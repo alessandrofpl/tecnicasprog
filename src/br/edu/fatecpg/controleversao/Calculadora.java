6f6830b8540c8fec9835c8cb370babb44be894fa
Ale Lima <alessandrofpl@gmail.com>
2024-02-29 17:34:14 -0300

    Implementação dos métodos da calculadora

---
+++ src/br/edu/fatecpg/controleversao/Calculadora.java
@@ -0,0 +1,17 @@
+package br.edu.fatecpg.controleversao;
+
+public class Calculadora {
+	public double soma(double num1, double num2) {
+		return num1+num2;
+	}
+	public double subtrai(double num1, double num2) {
+		return num1-num2;
+	}
+	public double multiplica(double num1, double num2) {
+		return num1*num2;
+	}
+	public double dividi(double num1, double num2) {
+		return num1/num2;
+	}
+
+}
--- src/br/edu/fatecpg/controleversao/Principal.java
+++ src/br/edu/fatecpg/controleversao/Principal.java
@@ -1,14 +1,45 @@
 package br.edu.fatecpg.controleversao;
-
+import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá Mundo");
+		int operacao;
+		double num1,num2,resultado=0;
+		Scanner scan = new Scanner(System.in);
+		Calculadora calc = new Calculadora();
 		
-		Somar calc = new Somar();
-		System.out.println(calc.soma(20, 10)); 
+		while(true) {
+		System.out.println("Digite a operação desejada: \n 1-soma"
+				+ "\n 2-subtração \n 3-multiplicação \n 4-divisao \n 5-sair");
+		operacao = scan.nextInt();
+		if(operacao == 5) {
+			break;
+		}
+		System.out.println("Digite o primeiro valor: ");
+		num1 = scan.nextDouble();
+		System.out.println("Digite o segundo valor: ");
+		num2 = scan.nextDouble();
 		
+		switch(operacao) {
+		case 1:
+			resultado = calc.soma(num1, num2);
+		break;
+		case 2:
+			resultado = calc.subtrai(num1, num2);
+		break;
+		case 3:
+			resultado = calc.multiplica(num1, num2);
+		break;
+		case 4:
+			resultado = calc.dividi(num1, num2);
+		break;
+		}
+		
+			
+		System.out.println(resultado); 
+		
+		}
 	}
 
 }
